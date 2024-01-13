5ca26a3919306a0afe0dc7329012fb710feb190d
bdiggines <122548319+bdiggines@users.noreply.github.com>
2024-01-13 21:26:27 +0000

    add expel.py

---
+++ expel.py
@@ -0,0 +1,13 @@
+#Output XY Control
+#def output_position(location):
+#    if location 
+
+#def ethflush(clean_time):
+    #Flush the system with ethanol, checking for blockages
+    #Move output to waste 
+
+#def synthesis():
+    #Move to waste (if not)
+    #Set pressure pumps
+    #check flow rates
+    
